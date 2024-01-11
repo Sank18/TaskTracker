b5cae4711988c21752290a7c322539e3fe1ffb2b
Sankalp Jain <sankalpj35@gmail.com>
2024-01-11 19:56:23 +0530

    new changes

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/goals/'
+const API_URL = 'https://task-track-ayvs.onrender.com/api/goals/'
 
 // Create new goal
 const createGoal = async (goalData,completeTime,priority, token) => {
